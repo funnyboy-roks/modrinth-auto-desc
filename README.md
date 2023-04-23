eeb5c93e35cc0690962fa285cb4c2c37ed5c9c2e
funnyboy-roks <funnyboyroks@gmail.com>
2023-04-23 12:33:46 -0500

    Add frontmatter parsing

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
 # Modrinth Auto-Description
 
-Automatically update the description for a Modrinth project.
+Automatically update the description for a Modrinth project from a markdown
+file in the repo.
+
+## Front Matter
+
+If your readme contains front matter (either yaml or json), you can send
+additional data to the Modrinth API.
+
+All additional data can be found in the [Modrinth Docs](https://docs.modrinth.com/api-spec/#tag/projects/operation/modifyProject).  
+*Note: The `body` key should not be specified and will be ignored if it is.*
+
+### Front Matter Format:
+
+```md
+---
+{
+    "modrinth": {
+        "source_url": "https://github.com/funnyboy-roks/mapify"
+    }
+}
+---
+# Your content here...
+```
+
 
 ## Inputs
 
-### auth-token
+### `auth-token`
 
 **Required**
 
@@ -19,8 +42,7 @@ To get this, you need to:
 4. Put it in a [GitHub Secret](https://docs.github.com/en/actions/security-guides/encrypted-secrets)
 5. You're done!
 
-
-### slug
+### `slug`
 
 **Required**
 
@@ -31,7 +53,7 @@ This is the part that you can type in the "URL" box in the settings.
 You can also use the `id` which can be found under the project
 information panel.
 
-### readme
+### `readme`
 
 **Optional**, default = `README.md`
 
--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 import fetch from 'node-fetch';
 import fs from 'fs/promises';
 import actions from '@actions/core';
+import fm from 'yaml-front-matter';
 
 const main = async () => {
+    const auth = actions.getInput('auth-token');
+    const slug = actions.getInput('slug');
+
+    actions.info(`Loading ${actions.getInput('readme')}.`);
+    const readme = await fs.readFile(actions.getInput('readme'), 'utf-8');
+
+    let frontMatter = fm.safeLoadFront(readme);
+
+    // use this since it removes the frontmatter
+    const content = frontMatter.__content.trim();
+
+    // We only care about stuff in the `modrinth` section
+    const { modrinth } = frontMatter;
+
+    // Prevent anybody from attempting change the description body
+    //
+    // This is a little confusing, because there is a key for modrinth called `discription` and one called `body`.
+    // The `body` key is the one which controls the markdown description, while the `description` controls the short description shown under the name.
+    if (modrinth.body) {
+        // Give a warning, but still continue
+        actions.warning('Ignoring `modrinth.body` in the front matter.  This field should not be set.');
+    }
+
+    modrinth.body = content;
+
     try {
-        const auth = actions.getInput('auth-token');
-        const slug = actions.getInput('slug');
-
-        actions.info(`Loading ${actions.getInput('readme')}.`);
-        const readme = await fs.readFile(actions.getInput('readme'), 'utf-8');
-
         actions.info('Sending request to Modrinth...');
         // https://docs.modrinth.com/api-spec/#tag/projects/operation/modifyProject
         const req = await fetch(`https://api.modrinth.com/v2/project/${slug}`, {
             method: 'PATCH',
-            body: JSON.stringify({
-                body: readme,
-            }),
+            body: JSON.stringify(body),
             headers: {
                 Authorization: auth,
                 'content-type': 'Application/json',
@@ -28,19 +46,22 @@ const main = async () => {
 
         // If res is not empty, there has been an error.
         if (res) {
-            throw JSON.stringify(JSON.parse(res), null, 4);
+            // Throw a pretty printed version of the JSON
+            throw 'API Error: ' + JSON.stringify(JSON.parse(res), null, 4);
         }
 
         actions.info('Updated description successfully!');
     } catch (err) {
-        actions.error(err);
+        let help = [];
 
         // Try to be mildly helpful
         if (`${err}`.includes('no such file or directory')) {
-            actions.error('Did you add `uses: actions/checkout@v3` to your workflow?');
+            help.push('Did you add `uses: actions/checkout@v3` to your workflow?');
+            help.push(`Did you use the correct path in the config? Path specified: ${readme}`);
         }
 
-        actions.setFailed(`Action failed with error: ${err}`);
+        help = help.map(l => '\t' + l).join('\n');
+        actions.setFailed(`Action failed with error: ${err}.\n${help}`.trim());
     }
 };
 
